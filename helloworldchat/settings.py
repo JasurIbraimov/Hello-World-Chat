a5837aaccd6a8a36284251a339717f58a52cf76c
Jasur Ibraimov <ibraimov0409@mail.ru>
2021-08-12 00:59:26 +0600

    solution without sqlite3

--- helloworldchat/settings.py
+++ helloworldchat/settings.py
@@ -116,6 +116,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
