30be294b49b599e30c593d741eb0b953b5b09326
Jasur Ibraimov <ibraimov0409@mail.ru>
2021-08-12 00:54:13 +0600

    added allowed host

--- helloworldchat/settings.py
+++ helloworldchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b9x04dp)kl$zr9c)*+i*i&_n-f+ilw04(n34!obg!a&-05xv!)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['helloworldchatty.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
